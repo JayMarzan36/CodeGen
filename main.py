fcfeb538e3f190f90f5fe7678e3243f97c41f85d
Jay Marzan <jaymarzan36@gmail.com>
2025-10-29 23:08:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from re import split
 import json
 
 import functions
@@ -10,24 +9,41 @@ initial_task_input = input("Give a task: ")
 # Ollama/model initialization
 model = "gemma3:4b"
 
-response = functions.prompt(model, initial_task_input)
+response = functions.prompt_model(model, initial_task_input)
 cleaned_response = response.replace("```", "").strip()
 cleaned_response = cleaned_response.replace("json", "")
 print(f"cleaned_response: {cleaned_response}")
-try:
-    data = json.loads(cleaned_response)
-    function_name = data["function_name"]
-    parameters = data["parameters"]
-    result = functions.execute_function(function_name, parameters)
 
-    if result:
-        print(result)
-    else:
-        pass
 
-except json.JSONDecodeError as e:
-    print(f"Error decoding response {e}")
-    print(f"Response: {cleaned_response}")
+result = None
+done = False
+iteration = 0
+while not done:
+    if iteration == 0:
+        result = initial_task_input
+
+    response = functions.prompt_model(model, result)
+    cleaned_response = response.replace("```", "").strip()
+    cleaned_response = cleaned_response.replace("json", "")
+    print(f"cleaned_response: {cleaned_response}")
+
+    try:
+
+        data = json.loads(cleaned_response)
+        function_name = data["function_name"]
+        parameters = data["parameters"]
+
+        if function_name == "print_to_user":
+            if parameters["message"] == '0':
+                done = True
+        else:
+            result = functions.execute_function(function_name, parameters)
+
+    except json.JSONDecodeError as e:
+        print(f"Error decoding response {e}")
+        print(f"Response: {cleaned_response}")
+
+    iteration += 1
 
 
 # while not task_done:
